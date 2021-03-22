a1078922130d9ecdd1af04f8d774bfd021d5c989
petercrawford <pcraw1027@gmail.com>
2021-03-21 20:37:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const element = document.querySelector("main#main");
+element.remove();
+
+let newHeader1 = document.createElement("h1");
+document.body.appendChild(newHeader1);
+newHeader1.id = "victory";
+newHeader1.innerHTML = "Peter is the champion";
+const newHeader = document.querySelector("#victory");
+
+
